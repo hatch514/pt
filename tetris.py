36b125e236b651345b0a5088978aca6f20010687
hatch514 <hr.katagiri@gmail.com>
2017-02-26 16:21:50 +0900

    Refacter the Coordinate

--- tetris.py
+++ tetris.py
@@ -1,191 +1,187 @@
-import pygame
+import pygame as pg
 import time, random
 import copy
 
-pygame.init()
-FPS=30
-
-display_height = 400
-display_width = 200
+pg.init()
+FPS = 30
 
 white = (255,255,255)
 black = (0,0,0)
 blue = (0,100,255)
 red = (255,0,0)
 
-block_size= 20
-startpoint_X=20*5
-startpoint_Y=0
-groundPoint=block_size*19
-rightwallPoint=block_size*9
-leftwallPoint=0
+blockSize =  20
+s_X = 5
+s_Y = 0
+groundPoint = 19
+rightWallPoint = 9
+leftWallPoint = 0
 
-gameDisplay = pygame.display.set_mode((display_width,display_height))
-pygame.display.set_caption('Simple TETRIS')
+DisplayHeight = blockSize * (groundPoint+1)
+DisplayWidth = blockSize * (rightWallPoint+1)
+gameDisplay = pg.display.set_mode((DisplayWidth,DisplayHeight))
+pg.display.set_caption('python TETRIS')
 # font setting
-pygame.font.init()
+pg.font.init()
   
-def frame():
-  pygame.draw.rect(gameDisplay, 
-          black, 
-          [display_width/2-(block_size*5),display_height/2-(block_size*10),block_size*10,block_size*20])
+def gameFrame():
+  pg.draw.rect(gameDisplay, black, [0, 0, DisplayWidth, DisplayHeight])
 
 def returnBlock():
   pass
   
 def startScreen():
   gameDisplay.fill(white)
-  fontTitle = pygame.font.SysFont("Consolas",20)
-  fontHowTo = pygame.font.SysFont("Consolas",12)
+  fontTitle = pg.font.SysFont("Consolas",20)
+  fontHowTo = pg.font.SysFont("Consolas",12)
   txtTitle = fontTitle.render('Py Tetris', False, black)
   txtHowTo = fontHowTo.render('press any key to start', False, black)
   gameDisplay.blit(txtTitle,(55,100))
   gameDisplay.blit(txtHowTo,(25,200))
-  pygame.display.update()
+  pg.display.update()
 
   screenLoop = True
   while screenLoop:
-    for event in pygame.event.get():
-      if event.type == pygame.KEYDOWN: 
+    for event in pg.event.get():
+      if event.type == pg.KEYDOWN:
         screenLoop = False  
-      if event.type == pygame.QUIT:
-        pygame.quit()
+      if event.type == pg.QUIT:
+        pg.quit()
         quit()    
   return 0
 
 def gameoverScreen():
   gameDisplay.fill(white)
-  fontTitle = pygame.font.SysFont("Consolas",20)
-  fontHowTo = pygame.font.SysFont("Consolas",12)
+  fontTitle = pg.font.SysFont("Consolas",20)
+  fontHowTo = pg.font.SysFont("Consolas",12)
   txtTitle = fontTitle.render('GAME OVER', False, red)
   txtHowTo = fontHowTo.render('press any key to go start screen', False, black)
   gameDisplay.blit(txtTitle,(55,100))
   gameDisplay.blit(txtHowTo,(25,200))
-  pygame.display.update()
+  pg.display.update()
 
   screenLoop = True
   while screenLoop:
-    for event in pygame.event.get():
-      if event.type == pygame.KEYDOWN: 
+    for event in pg.event.get():
+      if event.type == pg.KEYDOWN:
         screenLoop = False  
-      if event.type == pygame.QUIT:
-        pygame.quit()
+      if event.type == pg.QUIT:
+        pg.quit()
         quit()    
   return 0
  
 def moveBlock(nowBlock,direction):
-  for eachBlock_XnY in nowBlock: 
+  for block in nowBlock:
     if direction=="right":
-      eachBlock_XnY[0] = eachBlock_XnY[0]+block_size
-      eachBlock_XnY[1] = eachBlock_XnY[1]
+      block[0] = block[0] + 1
+      block[1] = block[1]
     if direction=="down":
-      eachBlock_XnY[0] = eachBlock_XnY[0]
-      eachBlock_XnY[1] = eachBlock_XnY[1]+block_size
+      block[0] = block[0]
+      block[1] = block[1] + 1
     if direction=="left":
-      eachBlock_XnY[0] = eachBlock_XnY[0]-block_size
-      eachBlock_XnY[1] = eachBlock_XnY[1]
+      block[0] = block[0] - 1
+      block[1] = block[1]
   return nowBlock
 
-def eval_ground(nowBlock,blockList):
-  for eachBlock_XnY in nowBlock:
-    if eachBlock_XnY[1] == groundPoint:
+def evalGround(nowBlock,blockList):
+  for block in nowBlock:
+    if block[1] == groundPoint:
       return True
     for eachFloorBlock in blockList: # There is a room for improvement
-      if eachBlock_XnY[0] == eachFloorBlock[0] and (eachBlock_XnY[1] + block_size)== eachFloorBlock[1] :
+      if block[0] == eachFloorBlock[0] and (block[1] + 1)== eachFloorBlock[1]:
         return True
   return False
 
-def eval_wall(nowBlock,blockList,direction):
+def evalWall(nowBlock,blockList,direction):
   if direction == "right":
-    for eachBlock_XnY in nowBlock:
-      if eachBlock_XnY[0] == rightwallPoint:
+    for block in nowBlock:
+      if block[0] == rightWallPoint:
         return True
       for eachWallBlock in blockList:
-        if eachBlock_XnY[0] + block_size == eachWallBlock[0] and eachBlock_XnY[1] == eachWallBlock[1]:
+        if (block[0] + 1) == eachWallBlock[0] and block[1] == eachWallBlock[1]:
           return True
           
   if direction == "left": 
-    for eachBlock_XnY in nowBlock:
-      if eachBlock_XnY[0] == leftwallPoint:
+    for block in nowBlock:
+      if block[0] == leftWallPoint:
         return True
       for eachWallBlock in blockList:
-        if eachBlock_XnY[0] - block_size == eachWallBlock[0] and eachBlock_XnY[1] == eachWallBlock[1]:
+        if (block[0] - 1) == eachWallBlock[0] and block[1] == eachWallBlock[1]:
           return True
         
   return False
 
 def evalGameOver(blockList):
-  for eachBlock in blockList:
-    if eachBlock[0] == startpoint_X and eachBlock[1] == startpoint_Y:
+  for block in blockList:
+    if block[0] == s_X and block[1] == s_Y:
       return False
   return True
 
-def initBlock():
-  nowBlock=[[startpoint_X,startpoint_Y],
-      [startpoint_X+0,startpoint_Y+block_size],
-      [startpoint_X+block_size,startpoint_Y+0],
-      [startpoint_X+block_size,startpoint_Y+block_size]]
-  return nowBlock
-  
+def drawNowBlock(nowBlock):
+  bs = blockSize
+  for block in nowBlock:
+    pg.draw.rect(gameDisplay,blue,[bs * block[0], bs * block[1], bs, bs])
+
 def drawAllBlock(blockList):
-  for eachBlock_XnY in blockList:
-    pygame.draw.rect(gameDisplay,blue,[eachBlock_XnY[0],eachBlock_XnY[1],block_size,block_size])
-  
+  bs = blockSize
+  for block in blockList:
+    pg.draw.rect(gameDisplay,blue,[bs * block[0], bs * block[1], bs, bs])
+
+def initBlock():
+  variety = ['square']
+  nextBlock = variety[0]
+  if nextBlock == 'square':
+    return [[s_X, s_Y], [s_X + 1, s_Y], [s_X, s_Y - 1], [s_X + 1,s_Y - 1]]
+
 def tetris():
   gameLoop = True
-  score=0
-  fallCount=0
-  clock=pygame.time.Clock()
-  blockList=[]
-  nowBlock=[[startpoint_X,startpoint_Y],
-      [startpoint_X+0,startpoint_Y+block_size],
-      [startpoint_X+block_size,startpoint_Y+0],
-      [startpoint_X+block_size,startpoint_Y+block_size]]
-      
+  score = 0
+  fallCount = 0
+  clock = pg.time.Clock()
+  blockList = []
+  nowBlock = initBlock()
+
   while gameLoop:
-    pygame.display.set_caption("Simple TETRIS : score "+str(score))
-    frame()
+    pg.display.set_caption("Simple TETRIS : score "+str(score))
+    gameFrame()
     clock.tick(FPS)
 
-    for event in pygame.event.get():
-      if event.type == pygame.QUIT:
-        pygame.quit()
+    for event in pg.event.get():
+      if event.type == pg.QUIT or event.key == pg.K_q:
+        pg.quit()
         quit()    
 
-    pygame.event.pump()
-    keys = pygame.key.get_pressed()
-    if keys[pygame.K_LEFT]:
-      if not eval_wall(nowBlock,blockList,"left"):
+    pg.event.pump()
+    keys = pg.key.get_pressed()
+    if keys[pg.K_LEFT]:
+      if not evalWall(nowBlock,blockList,"left"):
         nowBlock = moveBlock(nowBlock,"left")
-    elif keys[pygame.K_RIGHT]:
-      if not  eval_wall(nowBlock,blockList,"right"):
+    elif keys[pg.K_RIGHT]:
+      if not  evalWall(nowBlock,blockList,"right"):
         nowBlock = moveBlock(nowBlock,"right")
-    elif keys[pygame.K_UP]:
+    elif keys[pg.K_UP]:
       pass
-    elif keys[pygame.K_DOWN]:
-      if eval_ground(nowBlock,blockList):
+    elif keys[pg.K_DOWN]:
+      if evalGround(nowBlock,blockList):
         fallCount = 30
       else:
         nowBlock = moveBlock(nowBlock,"down")
           
-    pygame.draw.rect(gameDisplay,blue,[nowBlock[0][0],nowBlock[0][1],block_size,block_size])
-    pygame.draw.rect(gameDisplay,blue,[nowBlock[1][0],nowBlock[1][1],block_size,block_size])
-    pygame.draw.rect(gameDisplay,blue,[nowBlock[2][0],nowBlock[2][1],block_size,block_size])
-    pygame.draw.rect(gameDisplay,blue,[nowBlock[3][0],nowBlock[3][1],block_size,block_size])
+    drawNowBlock(nowBlock)
     drawAllBlock(blockList)
     fallCount+=1
     
     if fallCount >= 30:
-      if eval_ground(nowBlock,blockList):
-        for eachBlock_XnY in nowBlock:
-          blockList.append(copy.deepcopy(eachBlock_XnY))
+      if evalGround(nowBlock,blockList):
+        for block in nowBlock:
+          blockList.append(copy.deepcopy(block))
         nowBlock = initBlock()
-        fallCount=0
+        fallCount = 0
       else :
         nowBlock = moveBlock(nowBlock,"down")
-        fallCount=0
+        fallCount = 0
 
-    pygame.display.update()
+    pg.display.update()
     gameLoop = evalGameOver(blockList)
 
 if __name__ == "__main__":
